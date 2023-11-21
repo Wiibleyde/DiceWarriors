f527babf649353d1a67ec4ca7ab2c55f43b0b2bf
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-11-21 15:40:38 +0100

    Refactor Serializable class to support nested objects in from_dict method
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- serializable.py
+++ serializable.py
@@ -4,12 +4,21 @@ class Serializable:
         for key, value in self.__dict__.items():
             print(key, value)
             if hasattr(value, "to_dict"):
-                dict_repr[key] = value.to_dict()
+                dict_repr[value.__class__.__name__] = value.to_dict()
             else:
                 dict_repr[key] = value
         return dict_repr
     
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(**data)
-    
+        new_data = {}
+        for key, value in data.items():
+            if isinstance(value, dict):
+                class_ = globals().get(key)
+                if class_ is not None and issubclass(class_, Serializable):
+                    new_data[key.lower()] = class_.from_dict(value)
+                else:
+                    new_data[key] = value
+            else:
+                new_data[key] = value
+        return cls(**new_data)
